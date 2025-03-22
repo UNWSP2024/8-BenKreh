d5f24a47d6e70001229ad2b02bbcedad0a56069b
BenKreh <BenKrehbiel888@gmail.com>
2025-03-21 23:08:55 -0500

    Update program_1.py

--- program_1.py
+++ program_1.py
@@ -1,19 +1,12 @@
-# Program #1: Initials
-# Write a program that gets a string containing a person's first, middle, and last names, 
-# and displays their first, middle, and last initials.  
-# For example, if the user enters John William Smith, the program should display J. W. S.
+#Ben Krehbiel
+#3/17/2025
+#Split the name
 
-# Add your logic starting on line 11
+import re
 
-def initials_generator(personsName):
+def get_name():
+    name = input("Please enter your first name, middle initial, and last name: ")
+    name_separated = re.findall('[A-Z]', name)
+    print(name_separated)
 
-    personsInitials = ""
-    #    Add your logic here
-
-    return personsInitials.strip()
-
-personsName = input('Enter the users first, middle, and last name')
-
-initials = initials_generator(personsName)
-
-print(initials)
+get_name()
